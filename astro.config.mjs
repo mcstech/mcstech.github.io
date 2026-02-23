d746911dc19e32ed3deeda8a618d5223c3d0418d
Muhaimin CS <muhaimincs@gmail.com>
2026-02-23 21:42:07 +0800

    Update site URL in Astro config to use custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mcstech.github.io',
+  site: 'https://mcstech.dev',
   base: '/',
 });
