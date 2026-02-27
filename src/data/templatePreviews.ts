a568c5ba16d10bbc1802ee664560895f39c5dd7f
Muhaimin CS <muhaimincs@gmail.com>
2026-02-27 23:23:13 +0800

    chore: remove unused TemplatePage and templatePreviews files

--- src/data/templatePreviews.ts
+++
@@ -1,9 +0,0 @@
-import previewMainRockpaper from '../assets/templates/rockpaper/preview-1.png';
-import previewSecondaryRockpaper from '../assets/templates/rockpaper/preview-2.png';
-
-export const templatePreviews: Record<string, { main: string; secondary: string }> = {
-	rockpaper: {
-		main: previewMainRockpaper.src,
-		secondary: previewSecondaryRockpaper.src
-	}
-};
