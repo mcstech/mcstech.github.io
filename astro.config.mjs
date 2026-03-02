9cc4177610091482dfee184b1d7284ec4c589e95
Muhaimin CS <muhaimincs@gmail.com>
2026-03-02 16:08:09 +0800

    fix: correct output configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   site: 'https://mcstech.dev',
   base: '/',
-
+  output: "static",
   vite: {
     plugins: [tailwindcss()],
   },
